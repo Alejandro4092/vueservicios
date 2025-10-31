f4bab55e52ff7ca5ff8b7e6ea1347df3288e2741
Alejandro4092 <acobomarcos@gmail.com>
2025-10-31 09:32:39 +0100

    ruta para empleados oficio

--- src/Router.js
+++ src/Router.js
@@ -3,11 +3,14 @@ import HomeComponent from "./components/HomeComponent.vue";
 import EmpleadosDetails from "./components/EmpeadosDetails.vue";
 import CustomersComponent from "./components/CustomersComponent.vue";
 import CochesComponent from "./components/CochesComponent.vue";
+import EmpleadosOficios from "./components/EmpleadosOficios.vue";
 const myRoutes = [
     {path:"/",component:HomeComponent},
     {path:"/coches",component:CochesComponent},
     {path:"/empleadosdetails",component:EmpleadosDetails},
-    {path:"/customers",component:CustomersComponent}
+    {path:"/customers",component:CustomersComponent},
+    {path:"/empeadosoficio/:oficio",component:EmpleadosOficios},
+    
 ];
 const router = createRouter({
     history:createWebHistory(),
